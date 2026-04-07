7e65d4649ef4644a450bdf445b48b665c1eda860
Ritter <ritteralbany@gmail.com>
2026-04-07 02:47:15 -0400

    whitenoise

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
